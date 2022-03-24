a9a973862536f2831672629c360a7156936a1ba8
yaelz <yaelz@wix.com>
2022-03-24 20:45:14 +0200

    Revert "fetching movies working"
    This reverts commit 4dad0b9c86b66504956e1cf7c399d6c6a4f99ddd.

--- public/app.js
+++ public/app.js
@@ -1,26 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom'
 
 const e = React.createElement;
 
 const LikeButton = () => {
-    const [movies, setMovies] = useState({a: 1, b:0});
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const data = await fetch('/movies');
-            console.log('******* data: ', data);
-        }
-
-        fetchData()
-            .catch(console.error);
-    }, [])
-
     return (
-        <>
-            <div>{movies.a}</div>
-            <div>{movies.b}</div>
-        </>
+        <button onClick={() => console.log('YASSSSSSssssddddereee')}>
+            Like
+        </button>
     );
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: ["@babel/polyfill", "./public/app.js"],
+    entry: "./public/app.js",
     mode: "development",
     module: {
         rules: [
