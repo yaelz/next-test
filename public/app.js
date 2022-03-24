fda663be782037d31e86091b923e4e86eea822cc
yaelz <yaelz@wix.com>
2022-03-24 21:41:04 +0200

    movies showing with imaage, title and flex

--- public/app.js
+++ public/app.js
@@ -18,12 +18,16 @@ const App = () => {
 
 
     return (
-        <div>{movies && movies.map((movie) => (
-            <div key={movie.id}>
-                <div key={`title${movie.id}`}>{movie.title}</div>
-                <img key={`image${movie.id}`} src={movie.image}/>
+        <div style={{backgroundColor: "#00D7FF"}}>
+            <div>Explore your next Movies and TV shows</div>
+            <div style={{display: 'flex', flexWrap: 'wrap'}}>{movies && movies.map((movie) => (
+                <div key={movie.id}>
+                    <div key={`title${movie.id}`}>{movie.title}</div>
+                    <img key={`image${movie.id}`} src={movie.image}/>
+                </div>
+                ))}
             </div>
-        ))}</div>
+        </div>
     );
 }
 
