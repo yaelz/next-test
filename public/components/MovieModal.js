2ab242e589c6c0afe0ca43195625370beaccd379
yaelz <yaelz@wix.com>
2022-03-26 17:24:05 +0300

    Make html synopsis work with dompurify

--- public/components/MovieModal.js
+++ public/components/MovieModal.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Modal from "react-modal";
+import DOMPurify from 'dompurify';
 
 Modal.setAppElement('#my_app_container');
 
@@ -15,6 +16,9 @@ const customStyles = {
 };
 
 const MovieModal = ({movie, modalIsOpen, closeModal}) => {
+
+    console.log('******* movie.synopsis: ', movie.synopsis)
+    console.log('******* `${movie.synopsis}`: ', `${movie.synopsis}`)
     return (
         <Modal
             isOpen={modalIsOpen}
@@ -24,12 +28,16 @@ const MovieModal = ({movie, modalIsOpen, closeModal}) => {
         >
             <div style={{display: 'flex', flexDirection: 'row', maxWidth: '900px'}}>
                 <img style={{width: '400px', height: '600px'}} src={movie.image}/>
-                <div style={{display: 'flex', flexDirection: 'column', paddingLeft: '140px'}}>
-                    <div style={{font: 'normal normal bold 36px/42px Arial', textTransform: 'uppercase', width: '300px'}}>{movie.title}</div>
-                    <div style={{font: 'normal normal normal 24px/30px Georgia'}}>{movie.runtime}</div>
-                    <div style={{font: 'normal normal normal 24px/30px Georgia'}}>{`${movie.rating}/10`}</div>
-                    <div style={{font: 'normal normal normal 24px/30px Georgia'}}>{movie.synopsis}</div>
-                    <button style={{border: '1px solid #000000', borderRadius: '100px', opacity: 1, font: 'normal normal normal 16px/20px Georgia', width: '100%', marginTop: '19px', backgroundColor: 'transparent', textAlign: 'left', padding: '6px 12px'}} onClick={closeModal}>Back to list</button>
+                <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between', paddingLeft: '140px', width: '400px'}}>
+                    <div style={{display: 'flex', flexDirection: 'column'}}>
+                        <div style={{font: 'normal normal bold 36px/42px Arial', textTransform: 'uppercase', width: '300px'}}>{movie.title}</div>
+                        <div style={{font: 'normal normal normal 24px/30px Georgia'}}>{movie.runtime}</div>
+                        <div style={{font: 'normal normal normal 24px/30px Georgia'}}>{`${movie.rating}/10`}</div>
+                        <div style={{font: 'normal normal normal 24px/30px Georgia', width: '300px'}} dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(movie.synopsis)}}></div>
+                    </div>
+                    <div>
+                        <button style={{border: '1px solid #000000', borderRadius: '100px', opacity: 1, font: 'normal normal normal 16px/20px Georgia', width: '260px', marginTop: '19px', backgroundColor: 'transparent', textAlign: 'right', padding: '6px 12px'}} onClick={closeModal}>Back to list</button>
+                    </div>
                 </div>
             </div>
         </Modal>
