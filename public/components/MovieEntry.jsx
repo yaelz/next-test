7084265747a5940a9e45c2ce1e8a25d5387e01e8
yaelz <yaelz@wix.com>
2022-03-26 15:08:06 +0300

    Added Read More button

--- public/components/MovieEntry.jsx
+++ public/components/MovieEntry.jsx
@@ -2,10 +2,11 @@ import React from "react";
 
 const MovieEntry = ({movie}) => {
     return (
-        <div style={{maxWidth: '200px', paddingRight: '15px', margin: 'auto', textAlign: 'center', verticalAlign: 'center'}}>
+        <div style={{width: '200px', paddingRight: '15px', margin: 'auto', textAlign: 'center', verticalAlign: 'center'}}>
             <img src={movie.image}/>
             <div style={{font: 'normal normal normal 24px/30px Georgia', textTransform: 'titlecase', textAlign: 'start'}} key={`title${movie.id}`}>{`${movie.title} (${movie.released})`}</div>
             <div style={{font: 'normal normal normal 24px/30px Arial', textAlign: 'start'}}>{`${movie.rating}`}</div>
+            <button style={{border: '1px solid #000000', borderRadius: '100px', opacity: 1, font: 'normal normal normal 16px/20px Georgia', width: '100%', marginTop: '19px', backgroundColor: 'transparent', textAlign: 'left', paddingLeft: '12px'}}>Read more</button>
         </div>
     )
 }
