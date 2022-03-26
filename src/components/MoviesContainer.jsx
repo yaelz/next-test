278e537493aff69959777f8711d58a589768a94e
yaelz <yaelz@wix.com>
2022-03-26 19:18:08 +0300

    search by title WORKING

--- src/components/MoviesContainer.jsx
+++ src/components/MoviesContainer.jsx
@@ -5,6 +5,7 @@ import AllMovies from "./AllMovies";
 const MoviesContainer = () => {
     const [movies, setMovies] = useState([]);
     const [movieIdSearch, setMovieIdSearch] = useState('aaa');
+    const [movieTitleSearch, setMovieTitleSearch] = useState('aaa');
 
     useEffect(() => {
         const fetchData = async () => {
@@ -24,13 +25,13 @@ const MoviesContainer = () => {
         setMovies(response.data);
     }
 
-    // const searchByTitle = async word => {
-    //     const response = await axios.get(`/movies/title/${word}`);
-    //     console.log('********* response.data: ', response.data);
-    //     setMovies(response.data);
-    // }
-
-    console.log('*********** movieIdSearch: ', movieIdSearch)
+    const searchByTitle = async e => {
+        const whyIsThisTheValue = e.target[0].value;
+        console.log('********* e: ', whyIsThisTheValue);
+        const response = await axios.get(`/movies/title/${whyIsThisTheValue}`);
+        console.log('********* response.data: ', response.data);
+        setMovies(response.data);
+    }
 
     // const searchByYear = async word => {
     //     const response = await axios.get(`/movies/${movieId}`);
@@ -42,26 +43,24 @@ const MoviesContainer = () => {
         <>
             <form onSubmit={e => {e.preventDefault(); searchByMovieId()}}>
                 <label>
-                    Search by ID:
+                    Search By ID:
                     <input type="text" value={movieIdSearch} onChange={(e) => {
-                        console.log('============== e: ', e)
                         e.preventDefault();
                         setMovieIdSearch(e.target.value)
                     }} />
                 </label>
                 <input type="submit" value="Submit" />
             </form>
-            {/*<form onSubmit={e => {e.preventDefault(); searchByTitle(e)}}>*/}
-            {/*    <label>*/}
-            {/*        Search by ID:*/}
-            {/*        <input type="text" value={movieIdSearch} onChange={(e) => {*/}
-            {/*            console.log('============== e: ', e)*/}
-            {/*            e.preventDefault();*/}
-            {/*            setMovieIdSearch(e.target.value)*/}
-            {/*        }} />*/}
-            {/*    </label>*/}
-            {/*    <input type="submit" value="Submit" />*/}
-            {/*</form>*/}
+            <form onSubmit={e => {e.preventDefault(); searchByTitle(e)}}>
+                <label>
+                    Search By Title:
+                    <input type="text" value={movieTitleSearch} onChange={(e) => {
+                        e.preventDefault();
+                        setMovieTitleSearch(e.target.value)
+                    }} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
             <AllMovies movies={movies}></AllMovies>
         </>
     );
